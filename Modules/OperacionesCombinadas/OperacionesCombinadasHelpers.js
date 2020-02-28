dc6551fd9737b63788ce202e1b7f7580d07546f7
DiegoGarcia <diego.93.paint@gmail.com>
2020-02-28 20:23:15 +0100

    format

--- Modules/OperacionesCombinadas/OperacionesCombinadasHelpers.js
+++ Modules/OperacionesCombinadas/OperacionesCombinadasHelpers.js
@@ -1,32 +1,32 @@
-class OperacionesCombinadasHelpers{
+class OperacionesCombinadasHelpers {
 
-    static crearSuma(dimension){
-        var operacion=[];
+    static crearSuma(dimension) {
+        var operacion = [];
         operacion.push(Math.round(Math.random() * 10 ** dimension));
         operacion.push("+");
         operacion.push(Math.round(Math.random() * 10 ** dimension));
         console.log(operacion);
         return operacion;
     }
-      
-    static crearResta(dimension){
-        var operacion=[];
+
+    static crearResta(dimension) {
+        var operacion = [];
         operacion.push(Math.round(Math.random() * 10 ** dimension));
         operacion.push("-");
         operacion.push(Math.round(Math.random() * 10 ** dimension));
         console.log(operacion);
         return operacion;
     }
-      
-    static crearProducto(dimension){
-        var operacion=[];
+
+    static crearProducto(dimension) {
+        var operacion = [];
         operacion.push(Math.round(Math.random() * 10 ** dimension));
         operacion.push("x");
         operacion.push(Math.round(Math.random() * 10 ** dimension));
         console.log(operacion);
         return operacion;
     }
-      
+
 }
 
-export {OperacionesCombinadasHelpers};
+export { OperacionesCombinadasHelpers };
