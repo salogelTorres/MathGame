7d67fb840027942820b26ba109c593464385a791
salogelTorres <luistorres@eletorres.com>
2020-02-28 10:16:33 +0100

    listo para frontend

--- Modules/OperacionesCombinadas/OperacionesCombinadasView.js
+++ Modules/OperacionesCombinadas/OperacionesCombinadasView.js
@@ -1,5 +1,4 @@
-// import { OperacionesCombinadasClass } from "./OperacionesCombinadasClass.js";
-
+import { OperacionesCombinadasClass } from "./OperacionesCombinadasClass.js";
 
 function aplicarOperacionesCombinadas(element) {
   if (element.length === undefined) {
@@ -13,6 +12,10 @@ function aplicarOperacionesCombinadas(element) {
 
 function generateOperacionesCombinadas(element) {
   element.innerHTML = `Aqui va un operacion combinada`;
+  var operacion = "2 + 5 x 8 : 3 + [ 2 x ( 5 + 7 ) ]";
+
+  var operacionCombinada = new OperacionesCombinadasClass(operacion);
+  console.log(operacionCombinada);
 }
 
 export { aplicarOperacionesCombinadas };
