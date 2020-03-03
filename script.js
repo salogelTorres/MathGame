3f76acc0e57a895743afc88985b6651aac5fe66c
salogelTorres <luistorres@eletorres.com>
2020-03-03 11:45:57 +0100

    sa

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function asignarSumaEnColumnas() {
   aplicarSumaEnColumnas(element, 3, 3); // El primer argumento es el elemento al que se le aplicará el ejercicio, el segundo argumento dice el numero de sumandos que se quieren generar, y el tercer argumento indica el orden de magnitud de los numeros generados
 
   var element = document.querySelectorAll(".sumaEnColumna2");
-  aplicarSumaEnColumnas(element, 2, 9); // El primer argumento es el elemento al que se le aplicará el ejercicio, el segundo argumento dice el numero de sumandos que se quieren generar, y el tercer argumento indica el orden de magnitud de los numeros generados
+  aplicarSumaEnColumnas(element, 5, 4); // El primer argumento es el elemento al que se le aplicará el ejercicio, el segundo argumento dice el numero de sumandos que se quieren generar, y el tercer argumento indica el orden de magnitud de los numeros generados
 }
 asignarSumaEnColumnas();
 
