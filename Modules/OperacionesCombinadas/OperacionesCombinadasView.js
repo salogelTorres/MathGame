b97faf585f9624ced17d42380697dc5bd4a50b36
salogelTorres <luistorres@eletorres.com>
2020-03-03 12:48:05 +0100

    save

---
+++ Modules/OperacionesCombinadas/OperacionesCombinadasView.js
@@ -0,0 +1,21 @@
+import { OperacionesCombinadasClass } from "./OperacionesCombinadasClass.js";
+import { OperacionesCombinadasHelpers } from "./OperacionesCombinadasHelpers.js";
+
+function aplicarOperacionesCombinadas(element) {
+  if (element.length === undefined) {
+    generateOperacionesCombinadas(element);
+  } else {
+    element.forEach(el => {
+      generateOperacionesCombinadas(el);
+    });
+  }
+}
+
+function generateOperacionesCombinadas(element) {
+  element.innerHTML = `Aqui va un operacion combinada`;
+  var text = new OperacionesCombinadasHelpers(3, 3, 3);
+  console.log(text);
+}
+
+
+export { aplicarOperacionesCombinadas };
