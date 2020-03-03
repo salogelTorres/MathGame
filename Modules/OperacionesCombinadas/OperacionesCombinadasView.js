8d8a3905d953f5a90c4480c433f516a5fe8632d0
salogelTorres <luistorres@eletorres.com>
2020-03-03 12:44:51 +0100

    save

--- Modules/OperacionesCombinadas/OperacionesCombinadasView.js
+++ Modules/OperacionesCombinadas/OperacionesCombinadasView.js
@@ -12,19 +12,7 @@ function aplicarOperacionesCombinadas(element) {
 }
 
 function generateOperacionesCombinadas(element) {
-  var print = ``;
-  var text =
-    "2 x [ 2 x 4 x ( 34 + 8 x 4 ) x ( 5 + 4 ) ] : 4 + [ 23 x ( 4 + 6 ) ]";
-  var operacionCombinada = new OperacionesCombinadasClass(text);
-  console.log(operacionCombinada);
-  // operacionCombinada.forEach(step => {
-  //   print += `<br>`;
-  //   step.forEach(char => {
-  //     print += `${char} `;
-
   
-  //   });
-  // });
   element.innerHTML = print;
 }
 
